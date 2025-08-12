3f5e1bd2351fd59b5c8f93ea7f7863ac828ecbb6
kiwaeva <kiwaeva@gmail.com>
2025-08-12 20:24:54 +0100

    Add data file to store all the images.

---
+++ src/components/data/cardsData.js
@@ -0,0 +1,23 @@
+import wednesdayImg from '../images/wednesday-addams.jpg';
+import enidImg from '../images/enid.jpg'
+import thingImg from '../images/thing.jpg'
+
+const cardsData = [
+    {
+      title: "Wednesday Addams",
+      image: wednesdayImg,
+      description: "Spooky but stylish.",
+    },
+    {
+        title: "Enid Sinclair",
+        image: enidImg,  // if using public/ folder
+        description: "Bright and bubbly werewolf.",
+      },
+      {
+        title: "Thing",
+        image: thingImg,
+        description: "Just a helpful hand.",
+      },
+    ];
+
+export default cardsData;
