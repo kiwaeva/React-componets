4d9877018190fc44393137a5d3b26dd8b13465ad
kiwaeva <kiwaeva@gmail.com>
2025-08-28 22:34:52 +0100

    Create Tabs component with clickable tab buttons.

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const MyTabs = ({children}) => {
+const [activeTab, setActiveTab] = useState(0);
+
+const tabButtons = children.map((tab,index)) => (
+    <button
+     key={index}
+     onClick={()=> setActiveTab(index)}
+     style={{
+        fontWeight: index === activeIndex ? 'bold' : 'normal',
+        marginRight: '8px',
+      }}
+    >
+
+    </button>
+)
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default MyTabs; 
